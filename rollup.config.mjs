15ca7380b0493311d0795bcd0a95b452631610fb
Zhanghao <yukiniro@hotmail.com>
2023-05-18 12:43:07 +0800

    chore: update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,34 +5,29 @@ import fileSize from "rollup-plugin-filesize";
 import typescript from "rollup-plugin-typescript2";
 import { readFile } from "fs/promises";
 
-function getConfig(libraryName) {
-  return {
-    input: "src/index.ts",
-    output: [
-      {
-        file: `./dist/${libraryName}.cjs`,
-        format: "cjs",
-      },
-      {
-        file: `./dist/${libraryName}.mjs`,
-        format: "esm",
-      },
-    ],
-    plugins: [
-      commonjs(),
-      nodeResolve(),
-      typescript(),
-      babel({
-        babelHelpers: "bundled",
-        presets: ["@babel/preset-env"],
-      }),
-      fileSize(),
-    ],
-  };
-}
-
 const json = await readFile("./package.json", "utf8");
 const libraryName = JSON.parse(json).name;
-const config = getConfig(libraryName);
 
-export default config;
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: `./dist/${libraryName}.cjs`,
+      format: "cjs",
+    },
+    {
+      file: `./dist/${libraryName}.mjs`,
+      format: "esm",
+    },
+  ],
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    typescript(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"],
+    }),
+    fileSize(),
+  ],
+};
